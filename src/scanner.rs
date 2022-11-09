40ebc6f9bcd87b3c70291b6586ac6c91e5094420
teldufalsari <idunaev71@gmail.com>
2022-11-09 15:42:35 +0300

    Rewrite scanner to use the new wrappers

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,202 +1,25 @@
-use std::{env, os::unix::prelude::RawFd, process, path::*, str::FromStr, fs};
+use std::{env, process};
 use std::collections::HashMap;
-use libc;
-use nix::{self, poll::{self, PollFlags}, errno::*};
-use fanotify::low_level::*;
+use nix;
+use nix::unistd::Pid;
+
 mod scanning;
-use scanning::scanning::*;
+mod fanotify_wrappers;
 
-/*
-  For some reason the library on crates.io
-  does not contain this definiton, but the open-source
-  on github.com does. I'll include lib sources
-  as a dependency in future.
-*/
-const FAN_OPEN_EXEC_PERM: u64 = 0x00040000;
+use scanning::*;
 
-/*
-  Rust doesn't have compile-time sizeof()'s like C does.
-  But don't you worry, this value is used only to
-  initialize a buffer that could fit approx. 200
-  of those structures, which are generally
-  variable-sized. In case presice size is
-  needed, runtime std::mem::size_of is used.
-*/
-const SIZEOF_FAN_METADATA: usize = 24;
-
-
-
-fn _fanotify_respond(fd: i32, resp_fd: i32, resp_response: u32) -> Result<usize, Errno> {
-    let mut response = resp_fd.to_ne_bytes().to_vec();
-    let mut code = resp_response.to_ne_bytes().to_vec();
-    response.append(&mut code);
-    nix::unistd::write(fd, &response)
-}
-
-fn decimate_mercilessly(pid: i32) {
-    // First remove the executable (at least try)
-    println!("Found malicious process, PID={}", pid);
-    let mut link_to_exe = PathBuf::from_str("/proc").unwrap();
-    link_to_exe.push(pid.to_string());
-    link_to_exe.push("exe");
-    let path_to_exe = fs::read_link(link_to_exe).unwrap(); // need to check unwrap
-    println!("Removing execulable \"{}\"...", path_to_exe.display());
-    match fs::remove_file(path_to_exe) {
-        Ok(_) => println!("Done."),
-        Err(_) =>  println!("It's invincible!"),
-    };
-    // Then kill the wrongdoer
-    println!("Killing process now...");
-    match nix::sys::signal::kill(nix::unistd::Pid::from_raw(pid), nix::sys::signal::SIGKILL) {
-        Ok(_) => println!("Done"),
-        Err(_) => println!("I can't even kill it. Why?..."),
-    }
-}
-
-fn handle_event(metadata: &fanotify_event_metadata, _fd: i32, proc_table: &mut HashMap<i32, ProcStats>) {
-    /* Check that run-time and compile-time structures match. */
-    if metadata.vers != FANOTIFY_METADATA_VERSION {
-        println!("Mismatch of fanotify metadata version.");
-        process::exit(1);
-    }
-    /* metadata->fd contains either FAN_NOFD, indicating a
-    queue overflow, or a file descriptor (a nonnegative
-    integer). Here, we simply ignore queue overflow. */
-    if metadata.fd >= 0 {
-        if (metadata.mask & FAN_MODIFY) != 0 {
-            // Retirieve write statistics for that process
-            let proc_stats = match proc_table.get_mut(&metadata.pid) {
-                Some(val) => val,
-                None => {
-                    proc_table.insert(metadata.pid, ProcStats::new());
-                    proc_table.get_mut(&metadata.pid).unwrap()
-                }
-            };
-            if proc_stats.paths.is_empty() { // If it's a new process
-                // Retrieve the modified file name and add it to the paths vector
-                let mut procfd_path = PathBuf::from_str("/proc/self/fd").unwrap();
-                procfd_path.push(metadata.fd.to_string());
-                let file_name = fs::read_link(procfd_path).unwrap();
-                proc_stats.paths.push(file_name);
-            } else { // The process is already in the table
-                // Retirive the modified accessed file name
-                let mut procfd_path = PathBuf::from_str("/proc/self/fd").unwrap();
-                procfd_path.push(metadata.fd.to_string());
-                let path1buf = fs::read_link(procfd_path).unwrap();
-                if proc_stats.paths.contains(&path1buf) {
-                    // If process writes to the same file again, it's less suspicious
-                    if proc_stats.susness > 0 {proc_stats.susness -= 1}
-                } else {
-                    // The closer paths are, the more it's suspicious
-                    let path2 = proc_stats.paths.last().unwrap().as_path();
-                    match distance(path1buf.as_path(), path2) {
-                        Distance::Zero => {if proc_stats.susness > 0 {proc_stats.susness -= 1}},
-                        Distance::SameDir => proc_stats.susness += 2,
-                        Distance::NeigbourDirs => proc_stats.susness += 1,
-                        Distance::Far => ()
-                    };
-                    proc_stats.paths.push(path1buf);
-                }
-            }
-            if proc_stats.susness > CRITICAL_SUSNESS {
-                decimate_mercilessly(metadata.pid);
-                proc_table.remove(&metadata.pid);
-            }
-        }
-        close_fd(metadata.fd);
-    }
-
-}
-
-fn handle_events(fd: RawFd, info: &mut HashMap<i32, ProcStats>) {
-    /* Helper functions to deal with fanotify_event_metadata buffers */
-    fn fan_event_ok(meta: *const fanotify_event_metadata, len: usize) -> bool {
-        let size_of_struct = std::mem::size_of::<fanotify_event_metadata>();
-        unsafe {
-            len >= size_of_struct &&
-            (*meta).event_len as usize >= size_of_struct &&
-            (*meta).event_len as usize <= len
-        }
-    }
-    fn fan_event_next(meta: &fanotify_event_metadata, len: &mut usize) -> *const fanotify_event_metadata {
-        *len -= meta.event_len as usize;
-        let ptr: *const fanotify_event_metadata = meta;
-        unsafe { ptr.offset(meta.event_len as isize) }
-    }
-
-    let mut buf = [0u8; SIZEOF_FAN_METADATA * 200];
-    /* Loop while events can be read from fanotify file descriptor. */
-    loop {
-        /* Read some events. */
-        let mut len = match nix::unistd::read(fd, &mut buf) {
-            Ok(val) => val,
-            Err(Errno::EAGAIN) => 0,
-            Err(_) => {
-                println!("Cannot read fanotify");
-                process::exit(1)
-            }
-        };
-        /* Check if end of available data reached. */
-        if len == 0 { break; }
-        /* Point to the first event in the buffer. */
-        let mut metadata = buf.as_ptr() as *const fanotify_event_metadata;
-        /* Loop over all events in the buffer. */
-        while fan_event_ok(metadata, len) {
-            unsafe {
-                handle_event(&*metadata, fd, info);
-                /* Advance to next event. */
-                metadata = fan_event_next(&*metadata, &mut len);
-            }
-        }
-    }
-}
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
-    let mut operations_info: HashMap<i32, ProcStats> = HashMap::new();
-
-    /* Check mount point is supplied. */
+    // Check mount point is supplied.
     if argv.len() != 2 {
         println!("Usage: {} MOUNT", argv[0]);
         process::exit(1);
     }
-    println!("Listening to the directory {}", argv[1]);
-    /* Create the file descriptor for accessing the fanotify API. */
-    let fd = fanotify_init(
-        FAN_CLOEXEC | FAN_CLASS_PRE_CONTENT | FAN_NONBLOCK,
-        O_RDONLY | O_LARGEFILE)
-        .expect("fanotify_init failed");
-    fanotify_mark(fd,
-        FAN_MARK_ADD | FAN_MARK_MOUNT,
-        FAN_MODIFY,
-        AT_FDCWD,
-        argv[1].as_str())
-        .expect("fanotify_mark failed");
-    /* Prepare for polling. */
-    let mut fds = [
-        poll::PollFd::new(libc::STDIN_FILENO as RawFd, poll::PollFlags::POLLIN),
-        poll::PollFd::new(fd as RawFd, poll::PollFlags::POLLIN),
-    ];
-    println!("Listening for events");
-    /* This is the loop to wait for incoming events. */
-    loop {
-        let poll_num = match poll::poll(&mut fds, -1) {
-            Ok(val) => val,
-            Err(Errno::EINTR) => continue,
-            Err(_) => {
-                println!("poll failed");
-                process::exit(1);
-            }
-        };
-        if poll_num > 0 {
-            if fds[0].revents().unwrap_or(PollFlags::empty()).contains(PollFlags::POLLIN) {
-                break;
-            }
-            if fds[1].revents().unwrap_or(PollFlags::empty()).contains(PollFlags::POLLIN) {
-                /* Fanotify events are available. */
-                handle_events(fd, &mut operations_info);
-            }
-        }
-    }
+    // Create the file descriptor for accessing the fanotify API and prepare for polling.
+    let (fanotify, fds) = prepare_input(argv[1].as_str()).expect("Fanotify init");
+    println!("Listening for events.");
+    let proc_table: HashMap<Pid, ProcStats> = HashMap::new();
+    loop_until_input_recieved(fanotify, fds, proc_table).expect("Looping through events");
     println!("Listening for events stopped.");
 }
--- src/scanning.rs
+++ src/scanning.rs
@@ -1,73 +1,256 @@
-pub mod scanning {
-    use std::path::*;
-    use std::vec::Vec;
+use std::path::*;
+use std::vec::Vec;
+use std::{process, str::FromStr, fs};
+use std::os::unix::prelude::{RawFd, AsRawFd};
+use std::collections::HashMap;
+use nix::{self, errno::*};
+use nix::sys::signal;
+use nix::unistd::{self, Pid};
+use nix::poll::{poll, PollFd, PollFlags};
+use libc;
 
-     /**
-      This structure contains a field that measures how
-      suspicious this process is and stores all file paths
-      that were modified by this process
-     */
-    pub struct ProcStats {
-        pub susness : i32,
-        pub paths: Vec<PathBuf>,
-    }
+use crate::fanotify_wrappers::*;
 
-    impl ProcStats {
-        pub fn new() -> ProcStats {
-            ProcStats {
-                susness: 0,
-                paths: Vec::new(),
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PUBLIC ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/// Initialize fanotify instance with appropriate flags and marks
+/// and return it with the descriptors needed for `poll`.
+pub fn prepare_input(path: &str) -> nix::Result<(Fanotify, [PollFd; 2])> {
+    let fanotify = Fanotify::fanotify_init(
+        InitFlags::FAN_CLOEXEC | InitFlags::FAN_CLASS_PRE_CONTENT | InitFlags::FAN_NONBLOCK,
+        OpenFlags::O_RDONLY | OpenFlags::O_LARGEFILE)?;
+    fanotify.add_mark(MarkFlags::FAN_MARK_MOUNT,
+        EventFlags::FAN_MODIFY | EventFlags::FAN_OPEN_PERM | EventFlags::FAN_OPEN_EXEC_PERM,
+        libc::AT_FDCWD,
+        path)?;
+    let fds = [
+        PollFd::new(libc::STDIN_FILENO as RawFd, PollFlags::POLLIN),
+        PollFd::new(fanotify.as_raw_fd(), PollFlags::POLLIN),
+    ];
+    return Ok((fanotify, fds));
+}
+
+/// Process all fanotify events as they are available
+/// until some input from standard input is recieved.
+pub fn loop_until_input_recieved(
+    fanotify: Fanotify,
+    mut fds: [PollFd; 2],
+    mut proc_table: HashMap<Pid, ProcStats>
+) -> nix::Result<()> {
+    loop {
+        let poll_num = poll(&mut fds, -1)?;
+        if poll_num > 0 {
+            if fds[0].revents().unwrap_or(PollFlags::empty()).contains(PollFlags::POLLIN) {
+                break;
+            }
+            if fds[1].revents().unwrap_or(PollFlags::empty()).contains(PollFlags::POLLIN) {
+                // Fanotify events are available.
+                handle_events(fanotify, &mut proc_table)?;
             }
         }
     }
+    Ok(())
+}
 
-    pub enum Distance {
-        Zero,
-        SameDir,
-        NeigbourDirs,
-        Far,
-    }
 
-    /**
-      Max value of susness field that processes are allowed to have.
-      If this value is exceeded, the process is killed.
-     */
-    pub const CRITICAL_SUSNESS: i32 = 5; 
+/// This structure contains a field that measures how
+/// suspicious this process is and stores all file paths
+/// that were modified by this process.
+#[derive(Debug, Clone)]
+pub struct ProcStats {
+    pub susness : i32,
+    pub paths: Vec<PathBuf>,
+}
 
-    /**
-      Find "distance" between two absolute paths.
-     */
-    pub fn distance(path1: &Path, path2: &Path) -> Distance {
-        if path1.eq(path2) {
-            return Distance::Zero;
+impl ProcStats {
+    /// Creates an empty record with zero fields.
+    pub fn new() -> ProcStats {
+        ProcStats {
+            susness: 0,
+            paths: Vec::new(),
         }
-        let parent1 = path1.parent().unwrap();
-        let parent2 = path2.parent().unwrap();
-        if parent1.eq(parent2) {
-            return Distance::SameDir;
-        } else {
-            let pparent1 = parent1.parent();
-            let pparent2 = parent2.parent();
-            if pparent1 != None {
-                if pparent2 != None {
-                    if pparent1.unwrap().eq(pparent2.unwrap()) {
-                        return  Distance::NeigbourDirs;
-                    }
-                } else { // pparent2 = None
-                    if pparent1.unwrap().eq(parent2) {
-                        return  Distance::NeigbourDirs;
-                    }
+    }
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PRIVATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+enum Distance {
+    Zero,
+    SameDir,
+    NeigbourDirs,
+    Far,
+}
+
+/// Max value of susness field that processes are allowed to have.
+///  If this value is exceeded, the process is killed.
+const CRITICAL_SUSNESS: i32 = 5; 
+
+///  Find "distance" between two absolute paths.
+fn distance(path1: &Path, path2: &Path) -> Distance {
+    if path1.eq(path2) {
+        return Distance::Zero;
+    }
+    let parent1 = path1.parent().unwrap();
+    let parent2 = path2.parent().unwrap();
+    if parent1.eq(parent2) {
+        return Distance::SameDir;
+    } else {
+        let pparent1 = parent1.parent();
+        let pparent2 = parent2.parent();
+        if pparent1 != None {
+            if pparent2 != None {
+                if pparent1.unwrap().eq(pparent2.unwrap()) {
+                    return  Distance::NeigbourDirs;
                 }
-            } else {
-                // pparent1 = None, pparent2 is not None
-                // Because if both  pparent1 and pparent2 are None,
-                // parent1 and parent2 are both "/" and thus parent1.eq(parent2) is true,
-                // but it's not since it's checked above.
-                if parent1.eq(pparent2.unwrap()) {
+            } else { // pparent2 = None
+                if pparent1.unwrap().eq(parent2) {
                     return  Distance::NeigbourDirs;
                 }
             }
+        } else {
+            // pparent1 = None, pparent2 is not None
+            // Because if both  pparent1 and pparent2 are None,
+            // parent1 and parent2 are both "/" and thus parent1.eq(parent2) is true,
+            // but it's not since it's been already checked above.
+            if parent1.eq(pparent2.unwrap()) {
+                return  Distance::NeigbourDirs;
+            }
         }
-        Distance::Far
     }
+    Distance::Far
+}
+
+fn kill_process(pid: Pid) {
+    // First remove the executable (at least try)
+    println!("Found malicious process, PID={}", pid);
+    let mut link_to_exe = PathBuf::from_str("/proc").unwrap();
+    link_to_exe.push(pid.to_string());
+    link_to_exe.push("exe");
+    let path_to_exe = fs::read_link(link_to_exe).unwrap(); // need to check unwrap and print something like (cannot locate process executable)
+    println!("Removing execulable \"{}\"...", path_to_exe.display());
+    match fs::remove_file(path_to_exe) {
+        Ok(_) => println!("Done."),
+        Err(_) =>  println!("It's invincible!"),
+    };
+    // Then kill the wrongdoer
+    println!("Killing process now...");
+    match signal::kill(pid, signal::SIGKILL) {
+        Ok(_) => println!("Done"),
+        Err(_) => println!("I can't even kill it. Why?..."),
+    }
+}
+
+fn handle_open_perm(
+    metadata: &FanotifyEventMetadata,
+    fanotify: Fanotify,
+    proc_table: &mut HashMap<Pid, ProcStats>
+) -> nix::Result<()> {
+    let response = if let Some(process) = proc_table.get(&metadata.pid) {
+        if process.susness > CRITICAL_SUSNESS {
+            Response::FAN_DENY
+        } else {
+            Response::FAN_ALLOW
+        }
+    } else {
+        Response::FAN_ALLOW
+    };
+    fanotify.respond(metadata.fd, response)?;
+    Ok(())
+}
+
+fn handle_modify_event(
+    metadata: &FanotifyEventMetadata,
+    proc_table: &mut HashMap<Pid, ProcStats>
+) -> nix::Result<()> {
+    // Retirieve write statistics for that process
+    let proc_stats = match proc_table.get_mut(&metadata.pid) {
+        Some(val) => val,
+        None => {
+            proc_table.insert(metadata.pid, ProcStats::new());
+            proc_table.get_mut(&metadata.pid).unwrap()
+        }
+    };
+    if proc_stats.paths.is_empty() {
+        // If it's a new process, retrieve the modified
+        // file name and add it to the paths vector
+        let mut procfd_path = PathBuf::from_str("/proc/self/fd").unwrap();
+        procfd_path.push(metadata.fd.to_string());
+        let file_name = fs::read_link(procfd_path).unwrap(); // Don't unwrap!!!!!!!!!!!!
+        proc_stats.paths.push(file_name);
+    } else {
+        // If the process is already in the table, we need to
+        // retrieve the name of the file modified
+        let mut procfd_path = PathBuf::from_str("/proc/self/fd").unwrap();
+        procfd_path.push(metadata.fd.to_string());
+        let path1buf = fs::read_link(procfd_path).unwrap(); // Don't unwrap!!!!!!!!!!!!!!
+
+        if proc_stats.paths.contains(&path1buf) {
+            // If process writes to the same file again, it's less suspicious
+            if proc_stats.susness > 0 {proc_stats.susness -= 1}
+
+        } else {
+            // If the files are different, we decide how close they are to each other.
+            // The closer paths are, the more it's suspicious
+            let path2 = proc_stats.paths.last().unwrap().as_path();
+
+            match distance(path1buf.as_path(), path2) {
+                Distance::Zero => {if proc_stats.susness > 0 {proc_stats.susness -= 1}},
+                Distance::SameDir => proc_stats.susness += 2,
+                Distance::NeigbourDirs => proc_stats.susness += 1,
+                Distance::Far => ()
+            };
+
+            // Save this path for future analysis
+            proc_stats.paths.push(path1buf);
+        }
+    }
+    if proc_stats.susness > CRITICAL_SUSNESS {
+        kill_process(metadata.pid);
+        proc_table.remove(&metadata.pid);
+    }
+    Ok(())
+}
+
+fn handle_event(
+    metadata: &FanotifyEventMetadata,
+    fanotify: Fanotify,
+    proc_table: &mut HashMap<Pid, ProcStats>
+) -> nix::Result<()> {
+    // Check that run-time and compile-time structures match.
+    if metadata.vers != FANOTIFY_METADATA_VERSION {
+        eprintln!("Mismatch of fanotify metadata version.");
+        process::exit(1);
+    }
+    if metadata.fd >= 0 {
+        if metadata.mask.contains(EventFlags::FAN_OPEN_PERM) || metadata.mask.contains(EventFlags::FAN_OPEN_EXEC_PERM) {
+            handle_open_perm(metadata, fanotify, proc_table)?;
+        }
+        if metadata.mask.contains(EventFlags::FAN_MODIFY) {
+            handle_modify_event(metadata, proc_table)?;
+        }
+        unistd::close(metadata.fd).unwrap_or_default();
+    }
+    Ok(())
+}
+
+/// Read all available events from fanotify instance
+/// and handle them accordingly.
+pub fn handle_events(fanotify: Fanotify, proc_table: &mut HashMap<Pid, ProcStats>) -> nix::Result<()> {
+    // Loop while events can be read from fanotify file descriptor.
+    loop {
+        // Read some events.
+        let events = match fanotify.read_events() {
+            Ok(vec) => vec,
+            Err(Errno::EAGAIN) => break,
+            Err(code) => {
+                println!("Read from fanotify failed: {}", code.to_string());
+                process::exit(code as i32);
+            }
+        };
+        // Loop over all events in the buffer.
+        for event in events.iter() {
+            handle_event(event, fanotify, proc_table)?;
+        }
+    }
+    Ok(())
 }
